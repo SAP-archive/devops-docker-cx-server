052bf9b3ebff1f91620f7b740c9f9fa9f05ddd22
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-07-25 15:21:28 +0200

    configure test

--- run_cps_test_case.sh
+++ run_cps_test_case.sh
@@ -1,6 +1,8 @@
 docker build -t ppiper/jenkins-master jenkins-master
+docker build -t ppiper/jenkinsfile-runner jenkinsfile-runner
 pushd jenkinsfile-runner
-docker-compose --file docker-compose.test.yml build
-docker-compose --file docker-compose.test.yml run sut
-
+docker run -v //var/run/docker.sock:/var/run/docker.sock -v $(pwd):/workspace -v /tmp \
+ -e CX_INFRA_IT_CF_PASSWORD -e CX_INFRA_IT_CF_USERNAME -e BRANCH_NAME=master \
+ -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e HOST=$(hostname) \
+ ppiper/jenkinsfile-runner
 popd
