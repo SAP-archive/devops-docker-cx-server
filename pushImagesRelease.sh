89c8ef747d58097a62089505727d067fa2442f7a
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-10-01 13:44:47 +0200

    Update pushImagesRelease.sh

--- pushImagesRelease.sh
+++ pushImagesRelease.sh
@@ -3,9 +3,9 @@
 echo "Branch $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker push ppiper/jenkinsfile-runner:latest
-    docker push ppiper/jenkins-master:latest
-    docker push ppiper/cx-server-companion:latest
+#    docker push ppiper/jenkinsfile-runner:latest
+#    docker push ppiper/jenkins-master:latest
+#    docker push ppiper/cx-server-companion:latest
 fi
 
 if [ "$TRAVIS_TAG" ] ; then
