a02f072de355c5eb0d2fdf1ba01c71009c9b155e
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-10-05 12:47:15 +0200

    test

--- mvn-github-action/monolith/entrypoint.sh
+++ mvn-github-action/monolith/entrypoint.sh
@@ -1,6 +1,10 @@
 
+echo GITHUB_REF $GITHUB_REF
+
 mvn -Dmaven.repo.local=/github/workspace/maven_local_repo install --update-snapshots --batch-mode \
             --file /github/workspace/pom.xml -Dmaven.test.skip=true
 
 mvn -Dmaven.repo.local=/github/workspace/maven_local_repo test --update-snapshots --batch-mode \
             --file /github/workspace/unit-tests/pom.xml
+
+find /github/workspace
