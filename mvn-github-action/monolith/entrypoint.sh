91f8c4baf32fcb6f511d73d56a89deb870635034
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-10-05 11:17:40 +0200

    addmonolith

---
+++ mvn-github-action/monolith/entrypoint.sh
@@ -0,0 +1,6 @@
+
+mvn -Dmaven.repo.local=/github/workspace/maven_local_repo install --update-snapshots --batch-mode \
+            --file /github/workspace/pom.xml -Dmaven.test.skip=true
+
+mvn -Dmaven.repo.local=/github/workspace/maven_local_repo test --update-snapshots --batch-mode \
+            --file /github/workspace/unit-tests/pom.xml
