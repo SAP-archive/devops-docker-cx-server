4798381126bb20e835a8b87beaf0868fde548501
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-03-13 18:17:38 +0100

    fix docker

--- cx-server-companion/files/cx-server-companion.sh
+++ cx-server-companion/files/cx-server-companion.sh
@@ -7,9 +7,8 @@ readonly cxserver_companion_docker_image='ppiper/cx-server-companion'
 readonly container_port_http=8080
 readonly container_port_https=8443
 readonly network_name='cx-network'
-#Two times rev in combination with cut -f1 returns the last element of the cgroup which corresponds to the container id
-#Returns f323454ad3402f2513712 applied to 10:name=systemd:/docker/f323454ad3402f2513712
-readonly cxserver_companion_container_id="$(head -n 1 /proc/self/cgroup | rev | cut -d '/' -f1 | rev)"
+# cf https://gist.github.com/nmarley/0ee3e2cf14dabfb868eb818b8b30e7e0#gistcomment-2318045
+readonly cxserver_companion_container_id="$(awk -F/ '{ print $NF }' /proc/1/cpuset)"
 
 readonly cxserver_mount=/cx-server/mount
 readonly backup_folder="${cxserver_mount}/backup"
