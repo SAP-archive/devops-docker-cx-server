c122d69297541786ee37151ccd3ffb0e3c8a4048
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-03-18 11:47:39 +0100

    Lint

--- ci.sh
+++ ci.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -o errexit
+set -o nounset
+
+if [[ "$#" -ne 1 ]]; then
+	echo "Usage: $(basename "$0") (latest || v*)"
+	exit 1
+fi
 
 VERSION=$1
-DOCKERHUB_USER=$2
-DOCKERHUB_PASSWORD=$3
 
 if [[ ${VERSION} != latest ]] && [[ ${VERSION} != "v"* ]]; then
     echo "Error: Version must be 'latest' or 'v*'"
@@ -19,22 +23,23 @@ build_image() {
 
     if [ "x${VERSION}" = xlatest ]; then
         # Create a backup of the image to allow rollback in case of failure
-        docker pull ${TAG}:latest
-        docker tag ${TAG}:latest ${TAG}:backup-of-latest
+        docker pull "${TAG}":latest
+        docker tag "${TAG}":latest "${TAG}":backup-of-latest
     fi
 
     # Build the Release Candidate
-    docker build ${DIR} --tag ${TAG}:${VERSION}-RC
+    docker build "${DIR}" --tag "${TAG}":"${VERSION}"-RC
 }
 
 smoke_test() {
     # Test Jenkins master image
     docker run --name cx-jenkins-master-test --detach --publish 8080:8080 \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        ppiper/jenkins-master:${VERSION}-RC
+        ppiper/jenkins-master:"${VERSION}"-RC
     # Wait up to 5 minutes for Jenkins to be up and fail if it does not return 200 after this time
     docker exec cx-jenkins-master-test timeout 300 bash -c "until curl --fail http://localhost:8080/api/json; do sleep 5; done"
-    # Check for 'SEVERE' messages in Jenkins log (usually caused by plugin or configuration issues)
+    # Check for 'SEVERE' messages in Jenkins log (usually caused by a plugin or configuration issues)
+    # Note: This will exit with code 1 if a finding of SEVERE exists
     docker logs cx-jenkins-master-test 2> >(grep --quiet SEVERE)
     # Print plugin config
     docker exec cx-jenkins-master-test bash -c "curl http://localhost:8080/pluginManager/api/xml?depth=1"
@@ -43,14 +48,13 @@ smoke_test() {
 push_image() {
     local TAG=$1
 
-    docker tag ${TAG}:${VERSION}-RC ${TAG}:${VERSION}
+    docker tag "${TAG}":"${VERSION}"-RC "${TAG}":"${VERSION}"
 
     if [ "x${VERSION}" = xlatest ]; then
-        echo docker push ${TAG}:backup-of-latest
+        docker push "${TAG}":backup-of-latest
     fi
 
-#fixme echos
-    echo docker push ${TAG}:${VERSION}
+    docker push "${TAG}":"${VERSION}"
 }
 
 build_image ppiper/jenkins-master jenkins-master
