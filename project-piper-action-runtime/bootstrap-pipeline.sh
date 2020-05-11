75b6a58b3373cc91fd49c7eea01f640d28e69840
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-11 21:42:36 +0200

    Update bootstrap-pipeline.sh

--- project-piper-action-runtime/bootstrap-pipeline.sh
+++ project-piper-action-runtime/bootstrap-pipeline.sh
@@ -7,3 +7,20 @@ wget -O bin/piper --quiet https://github.com/SAP/jenkins-library/releases/latest
 chmod +x bin/piper
 echo "::add-path::${PWD}/bin"
 echo "##vso[task.setvariable variable=PATH]${PATH}:${PWD}/bin"
+
+
+if [ ! -f .pipeline/01_prepareVersion.sh ]; then
+    echo "piper artifactPrepareVersion" > .pipeline/01_prepareVersion.sh
+    chmod +x 01_prepareVersion.sh
+fi
+
+if [ ! -f .pipeline/02_build.sh ]; then
+    echo "piper mavenBuild" > .pipeline/02_build.sh
+    chmod +x 02_build.sh
+fi
+
+if [ ! -f .pipeline/04_staticChecks.sh ]; then
+    echo "piper mavenExecuteStaticCodeChecks" > .pipeline/04_staticChecks.sh
+    chmod +x 04_staticChecks.sh
+fi
+
