41c76537face73c97b7400940ed5a23dcceb2eea
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-11 21:46:36 +0200

    Update bootstrap-pipeline.sh

--- project-piper-action-runtime/bootstrap-pipeline.sh
+++ project-piper-action-runtime/bootstrap-pipeline.sh
@@ -8,19 +8,20 @@ chmod +x bin/piper
 echo "::add-path::${PWD}/bin"
 echo "##vso[task.setvariable variable=PATH]${PATH}:${PWD}/bin"
 
+mkdir -p .pipeline
 
 if [ ! -f .pipeline/01_prepareVersion.sh ]; then
     echo "piper artifactPrepareVersion" > .pipeline/01_prepareVersion.sh
-    chmod +x 01_prepareVersion.sh
+    chmod +x .pipeline/01_prepareVersion.sh
 fi
 
 if [ ! -f .pipeline/02_build.sh ]; then
     echo "piper mavenBuild" > .pipeline/02_build.sh
-    chmod +x 02_build.sh
+    chmod +x .pipeline/02_build.sh
 fi
 
 if [ ! -f .pipeline/04_staticChecks.sh ]; then
     echo "piper mavenExecuteStaticCodeChecks" > .pipeline/04_staticChecks.sh
-    chmod +x 04_staticChecks.sh
+    chmod +x .pipeline/04_staticChecks.sh
 fi
 
