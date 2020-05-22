cf93f62a600825559650b5e26a480a66b946af4e
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-22 09:43:58 +0200

    try single script

--- project-piper-action-runtime/bootstrap-pipeline.sh
+++ project-piper-action-runtime/bootstrap-pipeline.sh
@@ -10,70 +10,46 @@ echo "##vso[task.setvariable variable=PATH]${PATH}:${PWD}/bin"
 
 mkdir -p .pipeline
 
-if [ ! -f .pipeline/01_prepareVersion.sh ]; then
-    cat << EOF > .pipeline/01_prepareVersion.sh
-#!/usr/bin/env bash
-
-set -ex
-
-piper artifactPrepareVersion
-EOF
-    chmod +x .pipeline/01_prepareVersion.sh
-fi
-
-
-if [ ! -f .pipeline/02_build.sh ]; then
-    cat << EOF > .pipeline/02_build.sh
-#!/usr/bin/env bash
-
-set -ex
-
-piper mavenBuild
-EOF
-    chmod +x .pipeline/02_build.sh
-fi
-
-
-if [ ! -f .pipeline/03_tests.sh ]; then
-    cat << EOF > .pipeline/03_tests.sh
-#!/usr/bin/env bash
-
-set -ex
-
-piper mavenExecute --goals=verify
-
-EOF
-    chmod +x .pipeline/03_tests.sh
-fi
-
-
-if [ ! -f .pipeline/04_staticChecks.sh ]; then
-    cat << EOF > .pipeline/04_staticChecks.sh
-#!/usr/bin/env bash
-
-set -ex
-
-piper mavenExecuteStaticCodeChecks
-EOF
-    chmod +x .pipeline/04_staticChecks.sh
-fi
-
 if [ ! -f .pipeline/ci.sh ]; then
     cat << EOF > .pipeline/ci.sh
 #!/usr/bin/env bash
 
 set -ex
 
-# Pipeline script for running locally as an alternative to act
+build() {
+    echo piper build
+}
+test() {
+    echo piper test
+}
+check() {
+    echo piper check
+}
+security() {
+    echo piper security
+}
+deploy_artifact() {
+    echo piper deploy_artifact
+}
+deliver() {
+    echo piper deliver
+}
 
+if [ -f .pipeline/ci_extensions.sh ]; then
+    . .pipeline/ci_extensions.sh
+fi
 
-. .pipeline/01_prepareVersion.sh
+case \$1 in
+    build) echo build;;
+    test) echo test;;
+    check) echo check;;
+    security) echo security;;
+    deploy_artifact) echo deploy_artifact;;
+    deliver) echo deliver;;
+    *) echo foobar;;
+esac
 
-. .pipeline/02_build.sh
-
-. .pipeline/04_staticChecks.sh
 
 EOF
     chmod +x .pipeline/ci.sh
 fi
-
