93bfb6275ac1baa8a55079b4e7fd18c4747d6716
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-29 10:52:43 +0200

    wip

---
+++ project-piper-action-runtime/pipe.ts
@@ -0,0 +1,19 @@
+const { spawn } = require("child_process");
+
+const ls = spawn("/opt/piper", []);
+
+ls.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+});
+
+ls.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+});
+
+ls.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
